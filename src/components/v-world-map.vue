6b97f9a7f9e174490e8cb234f82b01b77f104bed
Александр Исаев <lexsandr46@rambler.ru>
2021-03-25 09:00:06 +0300

    First commit

---
+++ src/components/v-world-map.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="worldmap">
+    <vue-world-map countryStrokeColor="red" id="worldmap"  ></vue-world-map>
+  </div>
+</template>
+
+<script>
+import VueWorldMap from 'vue-world-map'
+export default {
+  name: 'v-world-map',
+  components: {
+    VueWorldMap
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
